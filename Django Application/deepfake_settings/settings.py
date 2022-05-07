02bcc8a81529b49acdfd0bf0b32518866b78e2f6
mehmoodyar.baig <yar.myb@gmail.com>
2022-05-07 23:24:59 +0600

    commit

--- Django Application/deepfake_settings/settings.py
+++ Django Application/deepfake_settings/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ml_app.apps.MlAppConfig'
+    'dfd_app.apps.MlAppConfig'
 ]
 
 MIDDLEWARE = [
--- Django Application/deepfake_settings/urls.py
+++ Django Application/deepfake_settings/urls.py
@@ -7,5 +7,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('ml_app.urls')),
+    path('', include('dfd_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
